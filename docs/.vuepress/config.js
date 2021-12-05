ebf9b5fc4dd36b4b17fcb4560bcf4a43177e1406
Xing Yahao <keidarcy.1015@gmail.com>
2021-12-05 14:30:52 +0900

    :tada: first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  lang: 'en-US',
+  title: 'Hello 🚀🚀',
+  description: 'This is blog, ',
+  bundler: '@vuepress/bundler-vite',
+  themeConfig: {
+    darkMode: true,
+    sidebar: false,
+    navbar: [
+      {
+        text: 'Github',
+        link: 'https://github.com/keidarcy',
+      },
+    ],
+  },
+};
---
+++ docs/README.md
@@ -0,0 +1,1 @@
+#### hey - something should be here :)
